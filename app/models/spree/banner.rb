4a8639a7e40b354f4100a72782268d814367c952
Aleksey <aleksey.dem@gmail.com>
2012-03-17 23:04:02 +0400

    Spree-style image url and path

--- app/models/spree/banner.rb
+++ app/models/spree/banner.rb
@@ -1,7 +1,12 @@
 module Spree
   class Banner < ActiveRecord::Base
     acts_as_list
-    has_attached_file :image, :styles => {:thumb => "100x100>" }
+    has_attached_file :image,
+      :styles => {:thumb => "100x100>" },
+      :default_style => :original,
+      :url => '/spree/banners/:id/:style/:basename.:extension',
+      :path => ':rails_root/public/spree/banners/:id/:style/:basename.:extension'
+
     default_scope :order => "position"
 
     scope :visible, where(:visible => true)
