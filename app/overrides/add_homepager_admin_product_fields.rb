6415edea8c045c19bc266b2d68a6907a004ea781
Aleksey <aleksey.dem@gmail.com>
2012-03-26 16:23:12 +0400

    Bugix. Add spree/ to partial path

--- app/overrides/add_homepager_admin_product_fields.rb
+++ app/overrides/add_homepager_admin_product_fields.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                     :name => "ext_homepager_admin_product_fields",
                     :insert_after => "[data-hook='admin_product_form_right']",
-                    :partial => "shared/ext_homepager_admin_product_fields.html",
+                    :partial => "spree/shared/ext_homepager_admin_product_fields.html",
                     :disabled => false)
